7217560d9458e3d8e360e60720c538297cbcb190
HONEY\nick5 <genghao2021@gmail.com>
2026-05-22 23:25:47 +0800

    調整例外 package 位置

--- backend/src/main/java/com/example/demo/iface/handler/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/demo/iface/handler/exception/GlobalExceptionHandler.java
@@ -5,8 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.example.demo.infra.shared.exception.exception.ResourceNotFoundException;
-import com.example.demo.infra.shared.exception.exception.ValidationException;
+import com.example.demo.infra.shared.exception.ResourceNotFoundException;
+import com.example.demo.infra.shared.exception.ValidationException;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
